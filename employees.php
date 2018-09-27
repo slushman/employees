7f1b643fd77c6062486d4f70a567be06a11407fc
Chris Wilcoxson <chris@slushman.com>
2018-09-27 14:35:14 -0500

    Change to current plugin development style.

--- employees.php
+++ employees.php
@@ -11,21 +11,25 @@
  *
  * @wordpress-plugin
  * Plugin Name:       Employees
- * Plugin URI:        http://slushman.com/employees
+ * Plugin URI:        http://www.slushman.com/employees
  * Description:       Creates a directory of employees
- * Version:           1.0.0
+ * Version:           1.5
  * Author:            Slushman
- * Author URI:        http://slushman.com
+ * Author URI:        http://www.slushman.com
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       employees
- * Domain Path:       /assets/languages
+ * Domain Path:       /languages
  */
 
+use Employees\Includes as Inc;
+use Employees\Admin as Admin;
+use Employees\Frontend as Front;
+use Employees\Fields as Fields;
+use Employees\Blocks as Blocks;
+
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
-}
+if ( ! defined( 'WPINC' ) ) { die; }
 
 // Used for referring to the plugin file or basename
 if ( ! defined( 'EMPLOYEES_FILE' ) ) {
@@ -33,46 +37,47 @@ if ( ! defined( 'EMPLOYEES_FILE' ) ) {
 }
 
 /**
- * The code that runs during plugin activation.
- * This action is documented in classes/class-activator.php
+ * Define plugin constants.
  */
-function activate_employees() {
-	require_once plugin_dir_path( __FILE__ ) . 'classes/class-activator.php';
-	Employees_Activator::activate();
-}
-
+define( 'EMPLOYEES_VERSION', '1.5' );
+define( 'EMPLOYEES_SLUG', 'employees' );
+define( 'EMPLOYEES_SETTINGS', 'employees_settings' );
 
 /**
- * The code that runs during plugin deactivation.
- * This action is documented in classes/class-deactivator.php
+ * Include the autoloader.
  */
-function deactivate_employees() {
-	require_once plugin_dir_path( __FILE__ ) . 'classes/class-deactivator.php';
-	Employees_Deactivator::deactivate();
-}
-
-register_activation_hook( __FILE__, 'activate_employees' );
-register_deactivation_hook( __FILE__, 'deactivate_employees' );
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-autoloader.php';
 
 /**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
+ * Activation and Deactivation Hooks.
  */
-require plugin_dir_path( __FILE__ ) . 'classes/class-employees.php';
+register_activation_hook( __FILE__, array( 'Employees\Includes\Activator', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'Employees\Includes\Deactivator', 'deactivate' ) );
 
 /**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
+ * Initializes each class and adds the hooks action in each to init.
  */
-function run_employees() {
+function employees_init() {
 
-	$plugin = new Employees();
-	$plugin->run();
+	$classes[] = new Inc\i18n();
+	$classes[] = new Admin\CPT_Employee();
+	$classes[] = new Admin\CPT_Employee_Columns();
+	$classes[] = new Admin\Taxonomy_Department();
+	$classes[] = new Admin\Metabox_Links( new Fields\Fields_Admin( 'metabox' ) );
+	$classes[] = new Admin\Metabox_Name( new Fields\Fields_Admin( 'metabox' ) );
+	$classes[] = new Admin\Metabox_Contact( new Fields\Fields_Admin( 'metabox' ) );
+	$classes[] = new Admin\Metabox_Job_Title( new Fields\Fields_Admin( 'metabox' ) );
+	$classes[] = new Admin\Metabox_Location( new Fields\Fields_Admin( 'metabox' ) );
+	$classes[] = new Admin\Metabox_Order( new Fields\Fields_Admin( 'metabox' ) );
+	$classes[] = new Admin\Admin( new Fields\Fields_Admin( 'settings' ), new Inc\Sanitize() );
+	$classes[] = new Blocks\Blocks();
 
-}
-run_employees();
+	foreach ( $classes as $class ) {
+
+		add_action( 'init', array( $class, 'hooks' ) );
+
+	}
+
+} // employees_init()
+
+add_action( 'plugins_loaded', 'employees_init' );
