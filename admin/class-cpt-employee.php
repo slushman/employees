3ac1dc97746743fa8b6ebfeb6d30bab99b2d7232
Chris Wilcoxson <chris@slushman.com>
2018-10-30 16:30:16 -0500

    Add contact info block to employee post template.

--- admin/class-cpt-employee.php
+++ admin/class-cpt-employee.php
@@ -82,7 +82,7 @@ class CPT_Employee {
 			array( 'core/heading', array( 'content' => __( 'Connect', 'employees' ), 'level' => 2 ) ),
 			array( 'social-links/social-links-block' ),
 			array( 'core/heading', array( 'content' => __( 'Contact', 'employees' ), 'level' => 2 ) ),
-			//array( 'employees/contact-info-block' ),			
+			array( 'employees/employee-contact-info-block' ),
 		);
 
 		$opts['labels']['add_new']						= esc_html__( 'Add New Employee', 'employees' );
